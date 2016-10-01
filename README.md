0eefd4548dca16732274468f9f154dba01886517
Maksim Kupriianov <max2k1@yandex.ru>
2016-10-01 19:38:48 +0500

    Import

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# rps
-RPS utility
+# RPS
+Utility to measure RPS (rows, or requests per second) and BPS for incoming from pipe flow.
+Primary usage: measure server's load, doing "tail -f" from access-logs.
+
+It reads from STDIN line-by-line (separated by '\n') and every second prints current RPS and BPS.
+When STDIN is closed, or program terminated by Ctrl-C, it prints summary information about input stream characteristics.
+
+Command-line arguments:
+```
+$ rps -h
+Usage of rps:
+  -noformat
+    	Do not format values
+  -nosummary
+    	Don't show summary at the end
+  -notime
+    	Don't show timestamp on everysecond stats
+  -oneline
+    	Print everysecond stats without newlines
+  -passthrough
+    	Passthrough incoming data to stdout
+```
+
+Usage:
+```
+$ tail -f /var/log/nginx/access.log | rps
+2016-10-01 19:12:58 205 RPS / 13,530 bytes
+2016-10-01 19:12:59 208 RPS / 13,728 bytes
+2016-10-01 19:13:00 208 RPS / 13,728 bytes
+2016-10-01 19:13:01 208 RPS / 13,728 bytes
+2016-10-01 19:13:02 207 RPS / 13,662 bytes
+2016-10-01 19:13:03 205 RPS / 13,530 bytes
+2016-10-01 19:13:04 208 RPS / 13,728 bytes
+2016-10-01 19:13:05 207 RPS / 13,662 bytes
+2016-10-01 19:13:06 199 RPS / 13,134 bytes
+^C= Summary: ========================
+Start:		2016-10-01 19:12:57
+Stop:		2016-10-01 19:13:06
+Elapsed, sec:	          9.043
+Bytes:		            122,958
+Speed, bps:	             13,596
+Lines:		              1,864
+MIN  RPS:	                199
+AVG  RPS:	                207
+50th RPS:	                207
+80th RPS:	                208
+95th RPS:	                208
+99th RPS:	                208
+MAX  RPS:	                208
+```
---
+++ rps.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	render_number "github.com/max2k1/render_number"
+	"io"
+	"math"
+	"os"
+	"os/signal"
+	"sort"
+	"sync/atomic"
+	"syscall"
+	"time"
+)
+
+var flagNoFormat bool
+var flagOneLine bool
+var flagPassthrough bool
+var flagDontShowSummary bool
+var flagDontShowTime bool
+
+func ReadLine(b *bufio.Reader) (line []byte, err error) {
+	// Use ReadSlice to look for array,
+	// accumulating full buffers.
+	var frag []byte
+	var full [][]byte
+
+R:
+	for {
+		var e error
+		frag, e = b.ReadSlice('\n')
+
+		switch {
+		case e == nil:
+			//got final fragment
+			break R
+		case e == bufio.ErrBufferFull:
+			// Make a copy of the buffer.
+			buf := make([]byte, len(frag))
+			copy(buf, frag)
+			full = append(full, buf)
+		default:
+			err = e
+			break R
+		}
+	}
+
+	// Allocate new buffer to hold the full pieces and the fragment.
+	n := 0
+	for i := range full {
+		n += len(full[i])
+	}
+	n += len(frag)
+
+	// Copy full pieces and fragment in.
+	buf := make([]byte, n)
+	n = 0
+	for i := range full {
+		n += copy(buf[n:], full[i])
+	}
+	copy(buf[n:], frag)
+	return buf, err
+}
+
+func init() {
+	flag.BoolVar(&flagNoFormat, "noformat", false, "Do not format values")
+	flag.BoolVar(&flagOneLine, "oneline", false, "Print everysecond stats without newlines")
+	flag.BoolVar(&flagPassthrough, "passthrough", false, "Passthrough incoming data to stdout")
+	flag.BoolVar(&flagDontShowSummary, "nosummary", false, "Don't show summary at the end")
+	flag.BoolVar(&flagDontShowTime, "notime", false, "Don't show timestamp on everysecond stats")
+	flag.Parse()
+}
+
+func main() {
+	var aRPS []float64 = make([]float64, 0, 600)
+	var bLine []byte
+	var err error = nil
+	var nBytes int64 = 0
+	var nBytesLastSec int64 = 0
+	var nLines int64 = 0
+	var nLinesLastSec int64 = 0
+
+	var nMinRPS int64 = -1
+	var n50RPS int64 = 0
+	var n80RPS int64 = 0
+	var n95RPS int64 = 0
+	var n99RPS int64 = 0
+	var nMaxRPS int64 = 0
+
+	// RPS ticker (lines per second)
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		var prefix string = ""
+		var val1 string = ""
+		var val2 string = ""
+		var res string = ""
+
+		for t := range ticker.C {
+			n := atomic.SwapInt64(&nLinesLastSec, 0)
+			b := atomic.SwapInt64(&nBytesLastSec, 0)
+
+			aRPS = append(aRPS, float64(n))
+			if n < nMinRPS || nMinRPS < 0 {
+				nMinRPS = n
+			}
+			if n > nMaxRPS {
+				nMaxRPS = n
+			}
+
+			prefix = ""
+			if !flagDontShowTime {
+				prefix = fmt.Sprintf("%s ", t.Format("2006-01-02 15:04:05"))
+			}
+
+			if flagNoFormat {
+				res = fmt.Sprintf("%s%d %d", prefix, n, b)
+			} else {
+				val1 = fmt.Sprintf("%s RPS", render_number.RenderInteger("#,###.", int64(n)))
+				val2 = fmt.Sprintf("%s bytes", render_number.RenderInteger("#,###.", int64(b)))
+				res = fmt.Sprintf("%s%s / %s", prefix, val1, val2)
+			}
+
+			if flagOneLine {
+				res = fmt.Sprintf("\r                                                                                \r%s", res)
+			} else {
+				res = fmt.Sprintf("%s\n", res)
+			}
+
+			fmt.Fprint(os.Stderr, res)
+		}
+	}()
+
+	// Read data through pipe line-by-line (\n-separated flow)
+	cInt := make(chan os.Signal, 2)
+	signal.Notify(cInt, os.Interrupt, syscall.SIGTERM)
+	b := bufio.NewReader(os.Stdin)
+	tStart := time.Now()
+	for err == nil {
+		select {
+		case <-cInt:
+			fmt.Fprintln(os.Stderr, "")
+			break
+		default:
+			bLine, err = ReadLine(b)
+			if err != nil && err != io.EOF {
+				panic(err)
+				break
+			}
+
+			nBytes += int64(len(bLine))
+			atomic.AddInt64(&nBytesLastSec, int64(len(bLine)))
+
+			nLines += 1
+			atomic.AddInt64(&nLinesLastSec, 1)
+			if flagPassthrough {
+				os.Stdout.Write(bLine)
+			}
+		}
+	}
+	tStop := time.Now()
+	ticker.Stop()
+
+	tElapsed := tStop.Sub(tStart)
+	if len(aRPS) > 0 {
+		if flagOneLine {
+			fmt.Fprintln(os.Stderr, "")
+		}
+		sort.Float64s(aRPS)
+		n50RPS = int64(aRPS[int64(math.Floor(float64(len(aRPS))*0.50))])
+		n80RPS = int64(aRPS[int64(math.Floor(float64(len(aRPS))*0.80))])
+		n95RPS = int64(aRPS[int64(math.Floor(float64(len(aRPS))*0.95))])
+		n99RPS = int64(aRPS[int64(math.Floor(float64(len(aRPS))*0.99))])
+	}
+
+	if !flagDontShowSummary {
+		fmt.Fprintln(os.Stderr, "= Summary: ========================")
+		fmt.Fprintf(os.Stderr, "Start:\t\t%s\n", tStart.Format("2006-01-02 15:04:05"))
+		fmt.Fprintf(os.Stderr, "Stop:\t\t%s\n", tStop.Format("2006-01-02 15:04:05"))
+		if flagNoFormat {
+			fmt.Fprintf(os.Stderr, "Elapsed, sec:\t%0.3f\n", float64(tElapsed.Seconds()))
+			fmt.Fprintf(os.Stderr, "Bytes:\t\t%d\n", nBytes)
+			fmt.Fprintf(os.Stderr, "Speed, bps:\t%0.0f\n", float64(nBytes)/tElapsed.Seconds())
+			fmt.Fprintf(os.Stderr, "Lines:\t\t%d\n", nLines)
+
+			if len(aRPS) > 0 {
+				fmt.Fprintf(os.Stderr, "MIN  RPS:\t%d\n", nMinRPS)
+				fmt.Fprintf(os.Stderr, "AVG  RPS:\t%0.0f\n", float64(nLines)/float64(len(aRPS)))
+				fmt.Fprintf(os.Stderr, "50th RPS:\t%d\n", n50RPS)
+				fmt.Fprintf(os.Stderr, "80th RPS:\t%d\n", n80RPS)
+				fmt.Fprintf(os.Stderr, "95th RPS:\t%d\n", n95RPS)
+				fmt.Fprintf(os.Stderr, "99th RPS:\t%d\n", n99RPS)
+				fmt.Fprintf(os.Stderr, "MAX  RPS:\t%d\n", nMaxRPS)
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "Elapsed, sec:\t%19s\n", render_number.RenderFloat("#,###.###", float64(tElapsed.Seconds())))
+			fmt.Fprintf(os.Stderr, "Bytes:\t\t%19s\n", render_number.RenderInteger("#,###.", int64(nBytes)))
+			fmt.Fprintf(os.Stderr, "Speed, bps:\t%19s\n", render_number.RenderInteger("#,###.", int64(float64(nBytes)/tElapsed.Seconds())))
+			fmt.Fprintf(os.Stderr, "Lines:\t\t%19s\n", render_number.RenderInteger("#,###.", int64(nLines)))
+
+			if len(aRPS) > 0 {
+				fmt.Fprintf(os.Stderr, "MIN  RPS:\t%19s\n", render_number.RenderInteger("#,###.", nMinRPS))
+				fmt.Fprintf(os.Stderr, "AVG  RPS:\t%19s\n", render_number.RenderFloat("#,###.", float64(nLines)/float64(len(aRPS))))
+				fmt.Fprintf(os.Stderr, "50th RPS:\t%19s\n", render_number.RenderInteger("#,###.", n50RPS))
+				fmt.Fprintf(os.Stderr, "80th RPS:\t%19s\n", render_number.RenderInteger("#,###.", n80RPS))
+				fmt.Fprintf(os.Stderr, "95th RPS:\t%19s\n", render_number.RenderInteger("#,###.", n95RPS))
+				fmt.Fprintf(os.Stderr, "99th RPS:\t%19s\n", render_number.RenderInteger("#,###.", n99RPS))
+				fmt.Fprintf(os.Stderr, "MAX  RPS:\t%19s\n", render_number.RenderInteger("#,###.", nMaxRPS))
+			}
+		}
+
+	}
+}
